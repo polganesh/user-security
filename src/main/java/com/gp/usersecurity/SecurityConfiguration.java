9b34c1759687dc5ef13f4842bef0496b02485ccf
Ganesh Pol <polganesh@gmail.com>
2017-08-03 21:30:42 +0530

    authorization logic

--- src/main/java/com/gp/usersecurity/SecurityConfiguration.java
+++ src/main/java/com/gp/usersecurity/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package com.gp.usersecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration //indicates it is configuration class. these classes loaded at application startup
 @EnableWebSecurity //security based annotation tells spring to enable web security for web application.
+@EnableGlobalMethodSecurity (prePostEnabled=true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	/**
--- src/main/java/com/gp/usersecurity/repository/UserRepository.java
+++ src/main/java/com/gp/usersecurity/repository/UserRepository.java
@@ -3,13 +3,20 @@
  */
 package com.gp.usersecurity.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.gp.usersecurity.entities.Usermaster;
 
 /**
+ * This repository has findAll methods available only to admin users
  * @author ganeshp
  *
  */
@@ -18,6 +25,36 @@ public interface UserRepository extends JpaRepository<Usermaster, Long> {
 	
 	
 	Usermaster findByUsername(@Param("username") String username);
+	//---------------------------------------------------------
+
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	default Page<Usermaster> findAll(Pageable arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	default List<Usermaster> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	default List<Usermaster> findAll(Iterable<Long> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	default List<Usermaster> findAll(Sort arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//----------------------------------------------------
 
 	
 }
